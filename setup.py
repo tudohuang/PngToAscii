9be15aba8bb3a72cbcba16313601bd337f20b907
TudoHuang <88125758+tudohuang@users.noreply.github.com>
2024-06-10 07:29:53 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
-    name="PngToAscii",
-    version="0.1",
-    packages=find_packages(),
+    name='PngToAscii',
+    version='0.1',
+    packages=['png_to_ascii'],
+    entry_points={
+        'console_scripts': [
+            'pngtoascii=png_to_ascii:main',
+        ],
+    },
     install_requires=[
-        "pillow"
+        'Pillow',
     ],
 )
