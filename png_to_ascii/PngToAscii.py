83afd699c9135c58b36a87f9c00bb111177e03ba
TudoHuang <88125758+tudohuang@users.noreply.github.com>
2024-06-10 11:46:44 +0800

    Update PngToAscii.py

--- png_to_ascii/PngToAscii.py
+++ png_to_ascii/PngToAscii.py
@@ -1,5 +1,6 @@
-import argparse
+import requests
 from PIL import Image
+from io import BytesIO
 
 def rgb_to_ansi(r, g, b):
     """
@@ -14,23 +15,35 @@ def rgb_to_ansi(r, g, b):
     """
     return f"\033[38;2;{r};{g};{b}m"
 
-def image_to_ascii(path, out_width=100, character_set=None):
+def fetch_image(source):
+    if source.startswith('http://') or source.startswith('https://'):
+        response = requests.get(source)
+        if response.status_code == 200:
+            img = Image.open(BytesIO(response.content))
+            return img
+        else:
+            print("Failed to retrieve image")
+            return None
+    else:
+        return Image.open(source)
+
+def image_to_ascii(source, out_width=100, character_set=None):
     if character_set is None:
         # 可自訂義
         character_set = "@%#WMN8B@WMmamwoc=;:-,. "
     
     char_len = len(character_set)
-    # 確保path安全
-    image_path = path.replace("\\", "/")
-    # RGB
-    img = Image.open(image_path).convert("RGB")
+    img = fetch_image(source)
+    if img is None:
+        return
+    
+    img = img.convert("RGB")
     width, height = img.size
     ratio = height / width
     out_height = int(out_width * ratio * 0.55)
     img = img.resize((out_width, out_height), Image.LANCZOS)
 
     pixels = list(img.getdata())
-    # 將灰度值轉換為字符
     grayscale_characters = [character_set[(r + g + b) // 3 * (char_len - 1) // 255] for (r, g, b) in pixels]
     ascii_image = [grayscale_characters[index: index + out_width] for index in range(0, len(grayscale_characters), out_width)]
     
@@ -39,13 +52,3 @@ def image_to_ascii(path, out_width=100, character_set=None):
             r, g, b = img.getpixel((x, y))
             print(f"{rgb_to_ansi(r, g, b)}{char}", end="")
         print("\033[0m")
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Convert an image to an ASCII art representation.")
-    parser.add_argument("path", type=str, help="The path to the image file.")
-    parser.add_argument("--width", type=int, default=100, help="The output width of the ASCII art (default: 100).")
-    parser.add_argument("--characters", type=str, default="@%#WMN8B@WMmamwoc=;:-,. ", help="The set of characters to use for ASCII art (default: '@%#WMN8B@WMmamwoc=;:-,. ').")
-
-    args = parser.parse_args()
-    
-    image_to_ascii(args.path, args.width, args.characters)
