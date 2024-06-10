f504cf51295771bd5ce768f97d447d3ac37bd8c3
TudoHuang <88125758+tudohuang@users.noreply.github.com>
2024-06-10 11:30:04 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,27 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
-    name='PngToAscii',
-    version='0.1',
-    packages=['png_to_ascii'],
+    name="png_to_ascii",
+    version="0.1",
+    packages=find_packages(),
     entry_points={
         'console_scripts': [
-            'pngtoascii=png_to_ascii:main',
+            'pngtoascii=png_to_ascii.__main__:main',
         ],
     },
     install_requires=[
-        'Pillow',
+        Pillow
+    ],
+    include_package_data=True,
+    license="MIT",
+    description="A package to convert images to ASCII art.",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url="https://github.com/tudohuang/PngToAscii",
+    author="Tudo Huang",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
     ],
 )
